3813e6331c0c785d54e089734ad1b4bf1bf97dcb
Sheng14 <1336840941@qq.com>
2021-09-12 15:53:50 +0800

    修复进化版Call中fn不被当成变量处理的问题

--- Javascript/call/fixCall.js
+++ Javascript/call/fixCall.js
@@ -5,9 +5,9 @@
 Function.prototype.myCall = function (context) {
     const fn = Symbol('fn')        // 声明一个独有的Symbol属性, 防止fn覆盖已有属性
     context = context || window // 若没有传入this, 默认绑定window对象
-    context.fn = this // 将函数挂载到对象的fn属性上
+    context[fn] = this // 将函数挂载到对象的fn属性上,注意需要以[]的方式挂载，不然fn不会被当做变量
     const args = [...arguments].slice(1) // 处理传入的参数
-    const result = context.fn(...args) // 通过对象的属性调用该方法
+    const result = context[fn](...args) // 通过对象的属性调用该方法
     delete context.fn // 删除该属性
     return result // 直接执行fn然后不保存到result不返回result也可以
 }
