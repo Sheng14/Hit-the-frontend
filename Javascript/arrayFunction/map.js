fc281d11b791d57295bf795c8e25d12852fb7bc4
Sheng14 <1336840941@qq.com>
2021-09-18 16:25:00 +0800

    手写一个更加优秀map

--- Javascript/arrayFunction/map.js
+++ Javascript/arrayFunction/map.js
@@ -14,3 +14,25 @@ Array.prototype.map = function (callback) {
 [1,2,3].map((item) => {
 	return item + 1;
 }) // [2, 3, 4]
+
+
+// ------ 更加优秀版------
+Array.prototype._map = function (fn, thisArr) {
+    if (this == undefined) {
+      throw new TypeError('this is null or not undefined');
+    }
+    if (Object.prototype.toString.call(fn) !== '[object Function]') {
+      throw new TypeError(fn + 'is not a function');
+    }
+    let res = [] 
+    let mapArr = this // [1, 2, 3]
+    for (let i = 0; i < mapArr.length; i++) {
+        res[i] = fn.call(thisArr, mapArr[i], i, mapArr)
+    }
+    return res
+}
+let arr = [1, 2, 3]
+let mapRes = arr._map((val, index, item) => {
+  return val + 1
+})
+console.log(mapRes)// [2,3,4]
